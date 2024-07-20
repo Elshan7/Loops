697ef5e9270f11d364e5e54bee3855851d4b2268
Elshan7 <elshanalizada1@gmail.com>
2024-07-20 12:19:09 +0400

    loops

---
+++ main.js
@@ -0,0 +1,97 @@
+//1 
+
+// for ( let i = 1; i <= 5; i++ ) { 
+//     for (let j = 1; j<=i; j++ ) {
+//         document.write("*");
+//     }
+
+//     document.write("<br/>");
+// }
+
+
+
+
+//2
+
+// for ( let i = 5; i >= 1; i-- ) {
+//     for ( let j = i; j >= 1; j-- ) {
+
+//         document.write("*");
+//     }
+
+//     document.write("<br/>");
+// }
+
+
+
+
+//3
+
+
+// function sum ( a, b ) {
+//     console.log (a + b); 
+// }
+
+// let result = sum (10,5 );
+
+
+// function substract ( a, b,c ) {
+//     console.log (a - b -c); 
+// }
+
+// let result2 = substract (10,5,1 );
+
+
+// function multiple (a,b) {
+//     return a * b;
+// }
+
+// let result3 = multiple (3,5);
+// console.log (result3);
+
+
+
+
+// function divide (a,b) {
+
+//     console.log ("a" + " /" + " b" + " =" + a / b);
+
+// }
+
+// divide (10,5);
+
+
+
+
+//4
+
+// let num = 5;
+// let fact = 1;
+
+// for ( let i = num ; i >=1; i--) {
+//     fact = fact * i;
+// }
+
+// console.log(fact);
+
+
+
+//5
+
+// let array= [10,1,2,3,8,9,99,77,44,5];
+
+// let max = array[0];
+
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] > max) {
+//         max = array[i];
+//     }
+// }
+
+// console.log(max);
+
+
+
+
+
